40c80edc26ef72438028fa5500b54d839ce427f1
root <root@chef1.ext.de1.eu.l7cache.net>
2024-03-07 23:27:44 +0000

    new gdns version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'szelcsanyi.gabor@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures zabbix, automatic client registration'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.26'
+version          '1.0.27'
 source_url       'https://github.com/szelcsanyi/chef-zabbix'
 issues_url       'https://github.com/szelcsanyi/chef-zabbix/issues'
 
